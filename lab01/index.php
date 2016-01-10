457cea7a94e755f751928ac73077f6f39000a748
Kelly Liu <kelly70545@hotmail.com>
2016-01-10 14:50:54 -0800

    KL: Added display, show_cell, and display functions

--- lab01/index.php
+++ lab01/index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <meta charset= "UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Tic Tac Toe Game</title>
     </head>
     <body>
@@ -11,9 +11,20 @@
         } else {
             $squares = '---------';
         }
-        if (winner('x', $squares)) {echo 'You win.';}
-        else if (winner('o', $squares)) {echo 'I win.';}
-        else {echo 'No winner yet.';}
+        $game = new Game($squares);
+        if ($game->winner('o')) {
+            echo "You win. Lucky guesses!";
+        } else if ($game->winner('x')) {
+            echo "I win. Muahahahaha";
+        } else {
+            $squares = $game->pick_move();
+            $game = new Game($squares);
+
+            if ($game->winner('x')) {
+                echo "I win. Muahahahaha";
+            }
+        }
+        $game->display();
         ?>
     </body>
 </html>
@@ -24,11 +35,12 @@ class Game {
 
     var $position;
 
-    function _construct($squares) {
+    function __construct($squares) {
         $this->position = str_split($squares);
     }
 
     function winner($token) {
+        $result = false;
         // Winning condition for horizontal lines
         for ($row = 0; $row < 3; $row++) {
             $result =  true;
@@ -66,6 +78,47 @@ class Game {
         if ($this->position[2] == $token && $this->position[4] == $token && $this->position[6] == $token) {
             $result = true;
         }
+        return $result;
     }
-    ?>
+
+    function show_cell($which) {
+        $token = $this->position[$which];
+        //deal with the easy case
+        if ($token <> '-') {
+            return '<td>' . $token . '</td>';
+        }
+        $this->newposition = $this->position; //copy the original
+        $this->newposition[$which] = 'o'; // opponent's move
+        $move = implode($this->newposition); // make a string from the board array
+        $link = 'index.php/?board=' . $move;
+        return '<td><a href="' . $link . '">-</a></td>'; //return a cell containing an anchor and showing a hyphen
+    }
+
+    function display() {
+        echo '<table cols=”3” style=”font­size:large; font­weight:bold”>';
+        echo '<tr>'; // open the first row
+        for ($pos = 0; $pos < 9; $pos++) {
+            echo $this->show_cell($pos);
+            if ($pos % 3 == 2) {
+                echo '</tr><tr>'; // close the table
+            }
+        }
+        echo '</tr>';
+        echo '</table>';
+    }
+    
+
+    function pick_move() {
+        $newposition = $this->position;
+        for ($pos = 0; $pos < 9; $pos++) {
+            if ($this->position[$pos] == '-') {
+                $newposition[$pos] = 'x';
+                $move = implode($newposition);
+                return $move;
+            }
+        }
+    }
+
+}
+?>
     
